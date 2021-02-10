ef35793ece70f0d10050606fa1e9dfb9c752c5b8
ragnarstefanss <ragnarstefanss@gmail.com>
2021-02-10 14:43:08 +0000

    When data processing has been done it will be saved to a new folder containing each csv

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ mot = pd.read_csv('csv/blak-mot.csv', sep=';', header=0)
 #print(lidsstjorar)
 print(mot)
 #print(thjalfarar)
+
+
+
+
+#FINAL STEP (run after everything is done):
+#save as new csv inside csv/new
+pd.DataFrame(domarar).to_csv("csv/new/blak-domarar.csv")
+pd.DataFrame(einstaklingar).to_csv("csv/new/blak-einstaklingar.csv")
+pd.DataFrame(forsvarsmenn).to_csv("csv/new/blak-forsvarsmenn.csv.csv")
+pd.DataFrame(lid).to_csv("csv/new/blak-lid.csv")
+pd.DataFrame(lidimoti).to_csv("csv/new/blak-lidimoti.csv")
+pd.DataFrame(lidsmenn).to_csv("csv/new/blak-lidsmenn.csv")
+pd.DataFrame(lidsstjorar).to_csv("csv/new/blak-lidsstjorar.csv")
+pd.DataFrame(mot).to_csv("csv/new/blak-mot.csv")
+pd.DataFrame(thjalfarar).to_csv("csv/new/blak-thjalfarar.csv")
